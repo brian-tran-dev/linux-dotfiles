6870ea50bf2780113d5948b388051b03ba43ecf9
Brian Dev <khoaphananhtran@gmail.com>
2025-12-29 01:24:29 +0700

    update fish

---
+++ private_dot_config/fish/functions/typst-compile.fish
@@ -0,0 +1,3 @@
+function typst-compile
+	typst compile --pdf-standard a-2u --jobs 2 --format pdf $argv
+end
---
+++ private_dot_config/fish/functions/typst-watch.fish
@@ -0,0 +1,3 @@
+function typst-watch
+	typst watch --pdf-standard a-2u --jobs 2 --format pdf $argv
+end
